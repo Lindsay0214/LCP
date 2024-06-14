5731ad6adf4e7380f7daa88699e04352a4db3172
Lindsay <lindsay.lui@cloudlytics.me>
2024-06-14 12:03:08 +0800

    dropWhile

---
+++ dropWhile.js
@@ -0,0 +1,28 @@
+function _dropWhile(array, predicate) {
+    let index = 0
+
+    while (index < array.length && predicate(array[index], index, array)) {
+        index++
+    }
+
+    return array.slice(index)
+}
+
+function dropWhile(array, predicate) {
+    let droppedCount = 0;
+
+    for (let i = 0; i < array.length; i++) {
+        if (!predicate(array[i], i, array)) {
+            break;
+        }
+        droppedCount++;
+    }
+
+    return array.slice(droppedCount);
+}
+
+
+
+dropWhile([1, 2, 3, 4, 5, 6], (value) => value < 4);
+dropWhile([0, 1, 2], (value) => value < 5);
+dropWhile([0, 6, 1, 2], (value) => value < 5)
